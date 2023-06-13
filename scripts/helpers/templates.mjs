1598ec68c2767046c66e60d75c2a975569e34803
Nekohime Musou <nekohime.musou@gmail.com>
2023-06-13 17:17:49 -0400

    Fix indents in html

--- scripts/helpers/templates.mjs
+++ scripts/helpers/templates.mjs
@@ -1,7 +1,12 @@
 export default async function preloadHandlebarsTemplates() {
   const templatePaths = [
     "systems/darksouls/templates/actor/parts/header.html",
-    "systems/darksouls/templates/actor/parts/tab-stats.html"
+    "systems/darksouls/templates/actor/parts/tab-stats.html",
+    "systems/darksouls/templates/actor/parts/stats/stats-pane.html",
+    "systems/darksouls/templates/actor/parts/stats/weapons-pane.html",
+    // "systems/darksouls/templates/actor/parts/stats/armor-pane.html",
+    // "systems/darksouls/templates/actor/parts/stats/rings-pane.html",
+    // "systems/darksouls/templates/actor/parts/stats/consumables-pane.html"
   ];
 
   return loadTemplates(templatePaths);
