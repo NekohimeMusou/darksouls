f8e7a754628a6cd0aeb85b871ad34c50d0179818
Nekohime Musou <nekohime.musou@gmail.com>
2023-06-19 12:58:23 -0400

    Fix indents in config.mjs

--- scripts/helpers/config.mjs
+++ scripts/helpers/config.mjs
@@ -1,12 +1,12 @@
 export const DARKSOULS = {};
 
 DARKSOULS.stats = {
-    vig: "DARKSOULS.Vigor",
-    atn: "DARKSOULS.Attunement",
-    vit: "DARKSOULS.Vitality",
-    str: "DARKSOULS.Strength",
-    dex: "DARKSOULS.Dexterity",
-    int: "DARKSOULS.Intelligence",
-    fth: "DARKSOULS.Faith",
-    luc: "DARKSOULS.Luck",
+  vig: "DARKSOULS.Vigor",
+  atn: "DARKSOULS.Attunement",
+  vit: "DARKSOULS.Vitality",
+  str: "DARKSOULS.Strength",
+  dex: "DARKSOULS.Dexterity",
+  int: "DARKSOULS.Intelligence",
+  fth: "DARKSOULS.Faith",
+  luc: "DARKSOULS.Luck",
 }
