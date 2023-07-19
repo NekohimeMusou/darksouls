03d053d84e8d4a6e394f52d1e87bac714180c3ad
Nekohime Musou <nekohime.musou@gmail.com>
2023-07-18 20:43:04 -0400

    Change weapons pane to damage pane

--- scripts/helpers/templates.mjs
+++ scripts/helpers/templates.mjs
@@ -4,7 +4,7 @@ export default async function preloadHandlebarsTemplates() {
     'systems/darksouls/templates/actor/parts/tab-stats.html',
     'systems/darksouls/templates/actor/parts/stats/stats-pane.html',
     'systems/darksouls/templates/actor/parts/stats/estus-pane.html',
-    'systems/darksouls/templates/actor/parts/stats/weapons-pane.html',
+    'systems/darksouls/templates/actor/parts/stats/damage-pane.html',
     'systems/darksouls/templates/actor/parts/stats/armor-pane.html',
     'systems/darksouls/templates/actor/parts/tab-armor.html'
   ];
