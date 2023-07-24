71de06e9ae5a2c754fde9feb023059ad29cbf1a8
Nekohime Musou <nekohime.musou@gmail.com>
2023-07-24 16:38:54 -0400

    Add effects tab (functional?)

--- scripts/helpers/templates.mjs
+++ scripts/helpers/templates.mjs
@@ -8,7 +8,8 @@ export default async function preloadHandlebarsTemplates() {
     'systems/darksouls/templates/actor/parts/stats/armor-pane.html',
     'systems/darksouls/templates/actor/parts/stats/defense-pane.html',
     'systems/darksouls/templates/actor/parts/stats/weight-pane.html',
-    'systems/darksouls/templates/actor/parts/tab-armor.html'
+    'systems/darksouls/templates/actor/parts/tab-armor.html',
+    'systems/darksouls/templates/actor/parts/tab-effects.html'
   ];
 
   return loadTemplates(templatePaths);
