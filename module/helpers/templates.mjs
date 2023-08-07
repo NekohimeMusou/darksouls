02bb1f5aa87f197fb54ad5f764376914aa5a9da3
Nekohime Musou <nekohime.musou@gmail.com>
2023-08-06 20:01:05 -0400

    Add weapons pane (basic)

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -5,6 +5,7 @@ export default async function preloadHandlebarsTemplates() {
     "systems/darksouls/templates/actor/parts/stats/stats-pane.html",
     "systems/darksouls/templates/actor/parts/stats/estus-pane.html",
     "systems/darksouls/templates/actor/parts/stats/damage-pane.html",
+    "systems/darksouls/templates/actor/parts/stats/weapons-pane.html",
     "systems/darksouls/templates/actor/parts/stats/armor-pane.html",
     "systems/darksouls/templates/actor/parts/stats/defense-pane.html",
     "systems/darksouls/templates/actor/parts/stats/weight-pane.html",
